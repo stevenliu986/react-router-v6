3100fb3c28c3df09e97b746a66fc653cab3c41b0
StevenLiu <stevenliu986@gmail.com>
2023-07-22 09:32:28 +1000

    modified:   src/App.jsx 	modified:   src/components/HomeHead.jsx 	new file:   src/views/A.jsx 	new file:   src/views/B.jsx 	new file:   src/views/C.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import HomeHead from "./components/HomeHead";
+// 导入需要的组件
+import A from "./views/A";
+import B from "./views/B";
+import C from "./views/C";
 function App() {
-  return <>React Router V6</>;
+  return (
+    <HashRouter>
+      <HomeHead />
+      <div className="content">
+        {/* 
+          所有的路由匹配规则，放在<Routes>中；每一条规则的匹配，是基于<Route>
+            路由匹配成功，不再基于component/render控制渲染的组件，
+            而是基于element，语法格式是<Component/>
+            不再需要Switch，默认就是一个匹配成功，就不再匹配下面的了
+            不再需要exact，默认每一项匹配都是精准匹配
+        */}
+        <Routes>
+          <Route path="/a" element={<A />} />
+          <Route path="/b" element={<B />} />
+          <Route path="/c" element={<C />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/HomeHead.jsx
+++ src/components/HomeHead.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const NavBox = styled.nav`
+  a {
+    margin-right: 10px;
+    color: #000;
+    &.active {
+      color: #ff0000;
+    }
+  }
+`;
 
 const HomeHead = () => {
-  return <div>HomeHead</div>;
+  return (
+    <NavBox>
+      <NavLink to="/a">A</NavLink>
+      <NavLink to="/b">B</NavLink>
+      <NavLink to="/c">C</NavLink>
+    </NavBox>
+  );
 };
 
 export default HomeHead;
