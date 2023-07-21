d1d388d5f4e639cabe9cfcd5a285d8cdcbe1d7fd
StevenLiu <stevenliu986@gmail.com>
2023-07-22 09:53:49 +1000

    modified:   index.html 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,29 @@ function App() {
             而是基于element，语法格式是<Component/>
             不再需要Switch，默认就是一个匹配成功，就不再匹配下面的了
             不再需要exact，默认每一项匹配都是精准匹配
+            原有的<Redirect>操作被<Navigate to="/" />代替
+              遇到<Navigate>组件，路由就会跳转到to指定的路由地址
+              设置replace属性，则不会新增立即记录，而是替换现有记录
+              to的值可以是一个对象：pathname需要跳转的地址，search问号传参信息
         */}
         <Routes>
-          <Route path="/" element={<Navigate to="/" />} />
+          <Route path="/" element={<Navigate to="/a" />} />
           <Route path="/a" element={<A />} />
           <Route path="/b" element={<B />} />
           <Route path="/c" element={<C />} />
+          {/* 如果所有路径都不匹配，就跳到指定的路由地址 */}
+          <Route path="*" element={<Navigate to="/a" />} />
+          {/* <Route
+            path="*"
+            element={
+              <Navigate
+                to={{
+                  pathname: "/a",
+                  search: "?id=100&name=hello",
+                }}
+              />
+            }
+          /> */}
         </Routes>
       </div>
     </HashRouter>
