6ff1b945342af45c32aca4dfe00c744573da8284
StevenLiu <stevenliu986@gmail.com>
2023-07-22 21:59:30 +1000

    modified:   src/router/routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,6 @@
+import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 import A from "../views/A";
-import B from "../views/B";
-import C from "../views/C";
 
 const routes = [
   {
@@ -10,9 +9,32 @@ const routes = [
   },
   // 注意：在component属性里面不能直接写成组件的形式，即<A />。
   // 如果写成<A />这样的形式，就意味着一旦程序运行就会直接调用A组件。
-  { path: "/a", name: "a", component: A, meta: {} },
-  { path: "/b", name: "b", component: B, meta: {} },
-  { path: "/c", name: "c", component: C, meta: {} },
+  { path: "/a", name: "a", component: A, meta: {}, children: [] },
+  {
+    path: "/b",
+    name: "b",
+    component: lazy(() => import("../views/B")),
+    meta: {},
+  },
+  {
+    path: "/c/:id?/:name?",
+    name: "c",
+    component: lazy(() => import("../views/C")),
+    meta: {},
+  },
+  {
+    path: "*",
+    component: () => {
+      return (
+        <Navigate
+          to={{
+            pathname: "/a",
+            search: "id=100&name=hello",
+          }}
+        />
+      );
+    },
+  },
 ];
 
 export default routes;
